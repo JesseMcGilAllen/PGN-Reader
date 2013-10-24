bbb07236d8c348f39ec2589196157a823d89dc8c
Jesse McGil Allen <jmallen7@gmail.com>
2013-10-24 16:16:49 -0500

    added Database Table View Controller

---
+++ JMADatabaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMADatabaseTableViewController.h
+//  PGN Reader
+//
+//  Created by Jesse McGil Allen on 10/24/13.
+//  Copyright (c) 2013 Jesse McGil Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMADatabaseTableViewController : UITableViewController
+
+@end
