641fe1e999116f822ed19fe609a8b134f5beb424
Jesse McGil Allen <jmallen7@gmail.com>
2013-11-08 21:45:17 -0600

    created square class

---
+++ PGN Reader/JMASquare.h
@@ -0,0 +1,13 @@
+//
+//  JMASquare.h
+//  PGN Reader
+//
+//  Created by Jesse McGil Allen on 11/8/13.
+//  Copyright (c) 2013 Jesse McGil Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMASquare : UIView
+
+@end
---
+++ PGN Reader/JMASquare.m
@@ -0,0 +1,31 @@
+//
+//  JMASquare.m
+//  PGN Reader
+//
+//  Created by Jesse McGil Allen on 11/8/13.
+//  Copyright (c) 2013 Jesse McGil Allen. All rights reserved.
+//
+
+#import "JMASquare.h"
+
+@implementation JMASquare
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
