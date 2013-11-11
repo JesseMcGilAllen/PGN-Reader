8812d3ac8b676f7f7aeac7f5f2c644e0d36005a2
Jesse McGil Allen <jmallen7@gmail.com>
2013-11-11 16:47:33 -0600

    added a piece class to the project

---
+++ PGN Reader/JMAPiece.h
@@ -0,0 +1,13 @@
+//
+//  JMAPiece.h
+//  PGN Reader
+//
+//  Created by Jesse McGil Allen on 11/11/13.
+//  Copyright (c) 2013 Jesse McGil Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMAPiece : UIView
+
+@end
---
+++ PGN Reader/JMAPiece.m
@@ -0,0 +1,31 @@
+//
+//  JMAPiece.m
+//  PGN Reader
+//
+//  Created by Jesse McGil Allen on 11/11/13.
+//  Copyright (c) 2013 Jesse McGil Allen. All rights reserved.
+//
+
+#import "JMAPiece.h"
+
+@implementation JMAPiece
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
