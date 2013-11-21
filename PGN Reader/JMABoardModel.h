512a9c7f88d3b8a0c766d71cbe04f869b2fb3d2a
Jesse McGil Allen <jmallen7@gmail.com>
2013-11-21 12:54:39 -0600

    added BoardModel class

---
+++ PGN Reader/JMABoardModel.h
@@ -0,0 +1,13 @@
+//
+//  JMABoardModel.h
+//  PGN Reader
+//
+//  Created by Jesse McGil Allen on 11/21/13.
+//  Copyright (c) 2013 Jesse McGil Allen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JMABoardModel : NSObject
+
+@end
---
+++ PGN Reader/JMABoardModel.m
@@ -0,0 +1,13 @@
+//
+//  JMABoardModel.m
+//  PGN Reader
+//
+//  Created by Jesse McGil Allen on 11/21/13.
+//  Copyright (c) 2013 Jesse McGil Allen. All rights reserved.
+//
+
+#import "JMABoardModel.h"
+
+@implementation JMABoardModel
+
+@end
